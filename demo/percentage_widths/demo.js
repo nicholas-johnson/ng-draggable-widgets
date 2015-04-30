495bd7256def90aa499b8cf30838e308282a82c3
Nicholas Johnson <nicholas@forwardadvance.com>
2015-04-30 15:45:26 +0100

    Additional demos

---
+++ demo/percentage_widths/demo.js
@@ -0,0 +1,33 @@
+(function() {
+
+  var widgets = [
+    {
+      title:'Cats with Woks',
+      class: 'woks'
+    },
+    {
+      title:'Socks on Sticks',
+      class: 'socks'
+    },
+    {
+      title:'Mocks of Macs',
+      class: 'mocks'
+    },
+    {
+      title:'Pops in Pumps',
+      class: 'pops'
+    },
+    {
+      title:'Hocks of Rumps',
+      class: 'hocks'
+    }
+  ];
+
+  angular.module('app', ['ng-draggable-widgets'])
+    .controller('dragController', function($scope) {
+      $scope.widgets = widgets;
+      $scope.moveWidget = function(drag) {
+        console.log(drag);
+      }
+    });
+})();
